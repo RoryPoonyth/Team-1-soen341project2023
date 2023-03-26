1013465927ea6ffd8b5680686de1512df4520550
patrickmac3 <patrickmaceachen9@gmail.com>
2023-03-26 17:57:43 -0400

    Application migration

---
+++ database/migrations/2023_03_26_190750_create_applications_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('applications', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->usignedBigInteger('listing_id');
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('resume');
+            $table->string('cover_letter')->nullable();
+            $table->timestamps();
+
+            $table->foreign('listing_id')->references('id')->on('listings')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('applications');
+    }
+};
