e59ccbb033842cb20a56048394e2c0f7c93c417f
patrickmac3 <patrickmaceachen9@gmail.com>
2023-03-29 16:15:29 -0700

    minor fixes

--- database/migrations/2023_03_26_190750_create_applications_table.php
+++ database/migrations/2023_03_26_190750_create_applications_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('applications', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
-            $table->usignedBigInteger('listing_id');
+            $table->unsignedBigInteger('listing_id');
             $table->string('name');
             $table->string('email');
             $table->string('phone');
