5884eab8ca81792accfa241c43e20bff0e661a90
patrickmac3 <patrickmaceachen9@gmail.com>
2023-04-01 13:34:18 -0700

    Header and application status view modified
    1. in the applications.show the status has been moved into the block of the application & visual effects for the different statuses were added.
    2. A dropdown was added into the header when logged in

--- resources/views/applications/show.blade.php
+++ resources/views/applications/show.blade.php
@@ -6,8 +6,9 @@
                     {{ $listing->title }}
                 </h2>
                 <div class="md:flex-grow mr-8 mt-2 flex items-center justify-start">
-                    @foreach($listing->tags as $tag)
-                        <span class="inline-block mr-2 tracking-wide text-indigo-500 text-xs font-medium title-font py-0.5 px-1.5 border
+                    @foreach ($listing->tags as $tag)
+                        <span
+                            class="inline-block mr-2 tracking-wide text-indigo-500 text-xs font-medium title-font py-0.5 px-1.5 border
                          border-indigo-500 uppercase">{{ $tag->name }}</span>
                     @endforeach
                 </div>
@@ -15,70 +16,89 @@
             <div class="-my-6">
                 <div class="flex flex-wrap md:flex-nowrap">
                     <div class="content w-full md:w-3/4 pr-4 leading-relaxed text-base">
-                            <div class="py-6 px-4 border-b border-gray-100 bg-white">
-                                {!! $listing->content !!}
-                            </div>
-                            @if (Auth::user()->is_employer)
-                                <h4 class="text-xl font-medium text-gray-900 title-font mt-5">
-                                    Applications ({{ $applications->count() }})
-                                </h4>
-                                @foreach ($applications as $application )
-                                        <div class="py-6 px-4 flex flex-wrap md:flex-nowrap border-b border-gray-100 bg-white hover:bg-gray-100">
-                                            <div class="md:w-16 md:mb-0 mb-6 mr-4 flex-shrink-0 flex flex-col">
-                                                <img src="/storage/{{ $listing->logo }}" alt="{{ $listing->company }}
-                                                logo" class="w-16 h-16 rounded-full object-cover">
-                                            </div>
-                                            <div class="md:w-1/2 mr-8 flex flex-col items-start justify-center">
-                                                <h2 class="text-xl font-bold text-gray-900 title-font mb-1">{{ $application->name }}</h2>
-                                                <p class="leading-relaxed text-gray-900">
-                                                    {{ $application->email }} &mdash; <span class="text-gray-600">{{ $application->status }}</span>
-                                                </p>
-                                            </div>
-                                            <div class="md:flex-grow mr-8 flex items-center justify-start">
-                                                <span class="inline-block ml-2 tracking-wide font-large title-font py-0.5 px-1.5 border border-indigo-500 uppercase bg-white text-indigo-500  hover:bg-indigo-500 hover:text-white">
-                                                    <a href="{{  Storage::url($application->resume) }}">resume</a>
-                                                </span>
-                                                <span class="inline-block ml-2 tracking-wide font-large title-font py-0.5 px-1.5 border border-indigo-500 uppercase bg-white text-indigo-500 hover:bg-indigo-500 hover:text-white">
-                                                    <a href="{{  route('applications.edit',['id' => $application->id ]) }}">Update status</a> 
-                                                </span>
-                                            </div>
-                                            <span class="md:flex-grow flex items-center justify-end">
-                                                <span>{{ $listing->created_at->diffForHumans() }}</span>
-                                            </span>
-                                        </div>
-                                @endforeach
-                            @else
-                                <h4 class="text-xl font-medium text-gray-900 title-font mt-5">
-                                    Application ({{ $application->status }})
-                                </h4>
-                                <div class="py-6 px-4 flex flex-wrap md:flex-nowrap border-b border-gray-100 bg-white hover:bg-gray-100">
+                        <div class="py-6 px-4 border-b border-gray-100 bg-white">
+                            {!! $listing->content !!}
+                        </div>
+                        @if (Auth::user()->is_employer)
+                            <h4 class="text-xl font-medium text-gray-900 title-font mt-5">
+                                Applications ({{ $applications->count() }})
+                            </h4>
+                            @foreach ($applications as $application)
+                                <div
+                                    class="py-6 px-4 flex flex-wrap md:flex-nowrap border-b border-gray-100 bg-white hover:bg-gray-100">
                                     <div class="md:w-16 md:mb-0 mb-6 mr-4 flex-shrink-0 flex flex-col">
-                                        <img src="/storage/{{ $listing->logo }}" alt="{{ $listing->company }}
-                                        logo" class="w-16 h-16 rounded-full object-cover">
+                                        <img src="/storage/{{ $listing->logo }}"
+                                            alt="{{ $listing->company }}
+                                                logo"
+                                            class="w-16 h-16 rounded-full object-cover">
                                     </div>
                                     <div class="md:w-1/2 mr-8 flex flex-col items-start justify-center">
-                                        <h2 class="text-xl font-bold text-gray-900 title-font mb-1">{{ $application->name }}</h2>
+                                        <h2 class="text-xl font-bold text-gray-900 title-font mb-1">
+                                            {{ $application->name }}</h2>
                                         <p class="leading-relaxed text-gray-900">
-                                            {{ $application->email }}
+                                            {{ $application->email }} &mdash; <span
+                                                class="text-gray-600">{{ $application->status }}</span>
                                         </p>
                                     </div>
                                     <div class="md:flex-grow mr-8 flex items-center justify-start">
-                                        <span class="inline-block ml-2 tracking-wide  font-large title-font py-0.5 px-1.5 border border-indigo-500 uppercase bg-white text-indigo-500">
-                                            <a href="{{  Storage::url($application->resume) }}">resume</a>
+                                        <span
+                                            class="inline-block ml-2 tracking-wide font-large title-font py-0.5 px-1.5 border border-indigo-500 uppercase bg-white text-indigo-500  hover:bg-indigo-500 hover:text-white">
+                                            <a href="{{ Storage::url($application->resume) }}">resume</a>
+                                        </span>
+                                        <span
+                                            class="inline-block ml-2 tracking-wide font-large title-font py-0.5 px-1.5 border border-indigo-500 uppercase bg-white text-indigo-500 hover:bg-indigo-500 hover:text-white">
+                                            <a href="{{ route('applications.edit', ['id' => $application->id]) }}">Update
+                                                status</a>
                                         </span>
                                     </div>
                                     <span class="md:flex-grow flex items-center justify-end">
                                         <span>{{ $listing->created_at->diffForHumans() }}</span>
                                     </span>
                                 </div>
-                            @endif
+                            @endforeach
+                        @else
+                            <h4 class="text-xl font-medium text-gray-900 title-font mt-5">
+                                Your application
+                            </h4>
+                            <div
+                                class="py-6 px-4 flex flex-wrap md:flex-nowrap border-b border-gray-100 bg-white hover:bg-gray-100">
+                                <div class="md:w-16 md:mb-0 mb-6 mr-4 flex-shrink-0 flex flex-col">
+                                    <img src="/storage/{{ $listing->logo }}"
+                                        alt="{{ $listing->company }}
+                                        logo"
+                                        class="w-16 h-16 rounded-full object-cover">
+                                </div>
+                                <div class="md:w-1/2 mr-8 flex flex-col items-start justify-center">
+                                    <h2 class="text-xl font-bold text-gray-900 title-font mb-1">
+                                        {{ $application->name }}</h2>
+                                    <p class="leading-relaxed text-gray-900">
+                                        {{ $application->email }}
+                                    </p>
+                                </div>
+                                <div class="md:flex-grow mr-8 flex items-center justify-start">
+                                    <span
+                                        class="inline-block ml-2 tracking-wide  font-large title-font py-0.5 px-1.5 border border-indigo-500 uppercase bg-white text-indigo-500">
+                                        <a href="{{ Storage::url($application->resume) }}">resume</a>
+                                    </span>
+                                    <span
+                                        class="inline-block ml-2 tracking-wide  font-large title-font py-0.5 px-1.5 border border-indigo-500 uppercase bg-white text-indigo-500
+                                        {{ $application->status === 'accepted'
+                                            ? 'bg-green-500 border-green-500 text-white'
+                                            : ($application->status === 'refused'
+                                                ? 'bg-rose-500 border-rose-500 text-white'
+                                                : 'bg-indigo-500 border-indigo-500 text-white') }}">
+                                        {{ $application->status }}
+                                    </span>
+                                </div>
+                                <span class="md:flex-grow flex items-center justify-end">
+                                    <span>{{ $listing->created_at->diffForHumans() }}</span>
+                                </span>
+                            </div>
+                        @endif
                     </div>
                     <div class="w-full md:w-1/4 pl-4">
-                        <img
-                            src="/storage/{{ $listing->logo }}"
-                            alt="{{ $listing->company }} logo"
-                            class="max-w-full mb-4"
-                        >
+                        <img src="/storage/{{ $listing->logo }}" alt="{{ $listing->company }} logo"
+                            class="max-w-full mb-4">
                         <p class="leading-relaxed text-base">
                             <strong>Location: </strong>{{ $listing->location }}<br>
                             <strong>Company: </strong>{{ $listing->company }}
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,41 +1,62 @@
 <header class="text-gray-600 body-font border-b border-gray-100">
     <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a href="{{ route('listings.index') }}" class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
+        <a href="{{ route('listings.index') }}"
+            class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round"
+                stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
+                viewBox="0 0 24 24">
                 <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
             </svg>
             <span class="ml-3 text-xl">Laravel Job Board</span>
         </a>
+
         @guest
             <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
                 <a href="{{ route('login') }}" class="mr-5 hover:text-gray-900">Login</a>
             </nav>
-            <a href="{{ route('register') }}" class="inline-flex items-center bg-indigo-500 text-white border-0 py-1 px-3 focus:outline-none hover:bg-indigo-600 rounded text-base mt-4 md:mt-0">Register
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
+            <a href="{{ route('register') }}"
+                class="inline-flex items-center bg-indigo-500 text-white border-0 py-1 px-3 focus:outline-none hover:bg-indigo-600 rounded text-base mt-4 md:mt-0">Register
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    class="w-4 h-4 ml-1" viewBox="0 0 24 24">
                     <path d="M5 12h14M12 5l7 7-7 7"></path>
                 </svg>
             </a>
         @else
-            @if (Route::is('dashboard'))
-                <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-                    <form action="{{ route('logout') }}" method="POST">
+        <div class="md:ml-auto">
+            <x-dropdown align="right" width="48" >
+                <x-slot name="trigger">
+                    <button
+                        class="py-2 pl-3 pr-9 text-lg font-medium text-gray-700 bg-white rounded-md hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100">
+                        {{ Auth::user()->name }}
+                        <span class="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                            <svg class="w-5 h-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                fill="currentColor" aria-hidden="true">
+                                <path fill-rule="evenodd" d="M10 14l-5-5 1.41-1.41L10 11.17l3.59-3.58L15 9l-5 5z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </span>
+                    </button>
+                </x-slot>
+                <x-slot name="content">
+                    <x-dropdown-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                        Dashboard
+                    </x-dropdown-link>
+                    @if (Auth::user()->is_employer)
+                        <x-dropdown-link :href="route('listings.create')" :active="request()->routeIs('listings.create')">
+                            Create Listing
+                        </x-dropdown-link>
+                    @endif
+                    <form method="POST" action="{{ route('logout') }}">
                         @csrf
-                        <button type="submit" class="mr-5 hover:text-gray-900">Logout</button>
+                        <x-dropdown-link :href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();">
+                            Logout
+                        </x-dropdown-link>
                     </form>
-                </nav>
-                @if (Auth::user()->is_employer)
-                    <a href="{{ route('listings.create') }}" class="inline-flex items-center bg-indigo-500 text-white border-0 py-1 px-3 focus:outline-none hover:bg-indigo-600 rounded text-base mt-4 md:mt-0">
-                        Create
-                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
-                            <path d="M5 12h14M12 5l7 7-7 7"></path>
-                        </svg>
-                    </a>
-                @endif
-            @else
-                <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-                    <a href="{{ route('dashboard') }}" class="mr-5 hover:text-gray-900">{{ Auth::user()->name }}</a>
-                </nav>
-            @endif
+                </x-slot>
+
+            </x-dropdown>
+        </div>
         @endguest
     </div>
 </header>
+
